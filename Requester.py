89d58b4a78d5dd814bb8702177c0372545b60f4b
chernyshvova <chernyshvova@.gmail.com>
2018-10-19 20:56:37 +0300

    created requester template

---
+++ Requester.py
@@ -0,0 +1,26 @@
+import urllib.request
+
+class request_HTTPconnector:
+    def __init__(self, proxy = None):
+        pass
+    def get_request(self):
+        pass
+    def set_request(self):
+        pass
+    def check_customeResponse(self):
+        pass
+    def reset_proxy(self, proxy):
+        pass
+    
+
+class web_Socket_Connector:
+    def __init__(self, proxy = None):
+        pass
+    def send(self):
+        pass
+    def receive(self):
+        pass
+    def reset_proxy(self, proxy):
+        pass
+    def check_custom_response(self):
+        pass
