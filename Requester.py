fee245b50c35bea3156de74796255027752f365d
chernyshvova <chernyshvova@.gmail.com>
2018-10-21 18:51:09 +0300

    added requsets func

--- Requester.py
+++ Requester.py
@@ -1,16 +1,26 @@
 import urllib.request
+from connector.HTTPTools import get_http_request, check_http_request
+from connector.proxy import proxy
 
 class request_HTTPconnector:
+    response = None
+    proxy = None
+
     def __init__(self, proxy = None):
-        pass
-    def get_request(self):
-        pass
+        if proxy:
+            self.proxy = proxy
+
+    def get_request(self, url):
+        response = get_http_request(url)
+        check_http_request(response)
+
     def set_request(self):
         pass
     def check_customeResponse(self):
         pass
+
     def reset_proxy(self, proxy):
-        pass
+        self.proxy = proxy
     
 
 class web_Socket_Connector:
